dda7f19c364c8d9cdd2e65fd00800825592e93df
fasil <fasil@dev-bee.com>
2022-01-18 20:33:32 +0530

    Added extension for number conversion

---
+++ Bee.NumberToWords/NumericWordsConvertorExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace Bee.NumberToWords
+{
+    public static class NumericWordsConvertorExtension
+    {
+        public static string ToWords(this decimal input)
+        {
+            return new NumericWordsConverter().ToWords(input);
+        }
+    }
+}
---
+++ WordConversionTests/NumericWordsConvertorExtensionTests.cs
@@ -0,0 +1,22 @@
+﻿using Bee.NumberToWords;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordConversionTests
+{
+    public class NumericWordsConvertorExtensionTests
+    {
+        [Test]
+        public void ShouldReturnEnglishTextWhenValidAmountIsPassed()
+        {
+            const decimal input = 5678.71M;
+            string actualResult = input.ToWords();
+            const string expectedResult = "Five thousand six hundred seventy eight point seven one";
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
